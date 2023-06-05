21cc29d9cbb92b110a5d62e065df70229780b302
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-06-05 09:56:50 +0530

    fix breadcrumb link path, added scss variables and some scss changes

--- pages/components/breadcrumb/breadcrumb.tsx
+++ pages/components/breadcrumb/breadcrumb.tsx
@@ -3,17 +3,18 @@ import Link from 'next/link';
 import React from 'react';
 
 export default function Breadcrumb() {
+
     const pathname = usePathname();
     const pathnames = pathname.split('/').filter((item) => item);
 
     return (
-        <div className="breadcrumb">
+        <div className="breadcrumb container">
             <p className="breadcrumb__text">
                 <span>UI/UX</span>
                 {pathnames.map((path, index) => (
                     <React.Fragment key={path}>
                         {" > "}
-                        <Link href={`/${path}`} className="breadcrumb__link">
+                        <Link href={`/${pathnames.slice(0, index + 1).join('/')}`} className="breadcrumb__link">
                             {path.replace(/-/g, " ").replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))}
                         </Link>
                     </React.Fragment>
